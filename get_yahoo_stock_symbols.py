8186aca7f222fcec9076450de955691fb94d4c0d
Giuseppe Valente <gvalente@arista.com>
2014-11-12 21:05:41 -0800

    Extract fetching code into a library
    - Create ysslib.py with all code unrelated to command usage
    - Generalize code for symbol length
    - Make company name extraction optional
    - Fix most pylint errors

--- get_yahoo_stock_symbols.py
+++ get_yahoo_stock_symbols.py
@@ -1,97 +1,9 @@
-from BeautifulSoup import BeautifulSoup
-import urllib2, time, getopt, sys
+#!/usr/bin/env python
 
-atotal = []
+import ysslib
+import getopt, sys, time
 
-def get_data(url):
-	try: v = urllib2.urlopen(url).read()
-	except: return ''
-	try: v = v.strip()
-	except: return ''
-	return v
-
-def get_group(a200, atotal):
-	s = '+'.join(a200)
-	f = 'sn'
-	url = 'http://finance.yahoo.com/d/quotes.csv?s=%s&f=%s' % (s, f)
-	v = get_data(url)
-	a = v.split("\r\n")
-	for s in a:
-		if s.find('N/A') == -1:
-			aa = s.split('"')
-			s = ''.join(aa)
-			aa = s.split(',')
-			try:
-				if aa[1] != '':
-					if aa[0] != aa[1]:
-						atotal.append(s)
-			except: continue
-
-def get_symbols(a1, atotal, verbose):
-	i = 0
-	j = 0
-	a200 = []
-	for e in a1:
-		try: a200.append(e)
-		except: break
-		i += 1
-		if i == 200:
-			j += 1
-			if verbose:
-				print j,
-				sys.stdout.flush()
-			get_group(a200, atotal)
-			a200 = []
-			i = 0
-	get_group(a200, atotal)
-	if verbose:
-		print "\n" + `len(atotal)`
-		sys.stdout.flush()
-
-def get_1_character_symbols(verbose):
-	global atotal
-	a1 = []
-	for i in range(65, 65+26):
-		a1.append(chr(i))
-	get_symbols(a1, atotal, verbose)
-
-def get_2_character_symbols(verbose):
-	global atotal
-	a1 = []
-	for i in range(65, 65+26):
-		for j in range(65, 65+26):
-			a1.append(chr(i)+chr(j))
-	get_symbols(a1, atotal, verbose)
-
-def get_3_character_symbols(verbose):
-	global atotal
-	a1 = []
-	for i in range(65, 65+26):
-		for j in range(65, 65+26):
-			for k in range(65, 65+26):
-				a1.append(chr(i)+chr(j)+chr(k))
-	get_symbols(a1, atotal, verbose)
-
-def get_4_character_symbols(verbose):
-	global atotal
-	a1 = []
-	for i in range(65, 65+26):
-		for j in range(65, 65+26):
-			for k in range(65, 65+26):
-				for l in range(65, 65+26):
-					a1.append(chr(i)+chr(j)+chr(k)+chr(l))
-	get_symbols(a1, atotal, verbose)
-
-def get_5_character_symbols(verbose):
-	global atotal
-	a1 = []
-	for i in range(65, 65+26):
-		for j in range(65, 65+26):
-			for k in range(65, 65+26):
-				for l in range(65, 65+26):
-					for m in range(65, 65+26):
-						a1.append(chr(i)+chr(j)+chr(k)+chr(l)+chr(m))
-	get_symbols(a1, atotal, verbose)
+# pylint: disable=W0312,C0301,W0311,W0622
 
 def help():
 	print 'to retrieve all 1-character symbols:'
@@ -113,7 +25,7 @@ def usage():
 
 def main():
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "vhc:", ["help", "characters="])
+		opts, _ = getopt.getopt(sys.argv[1:], "vhc:", ["help", "characters="])
 	except getopt.GetoptError, err:
 		print str(err)
 		usage()
@@ -136,33 +48,15 @@ def main():
 	if characters == None:
 		help()
 		sys.exit()
-	elif characters == '1':
-		get_1_character_symbols(verbose)
-	elif characters == '2':
-		get_1_character_symbols(verbose)
-		get_2_character_symbols(verbose)
-	elif characters == '3':
-		get_1_character_symbols(verbose)
-		get_2_character_symbols(verbose)
-		get_3_character_symbols(verbose)
-	elif characters == '4':
-		get_1_character_symbols(verbose)
-		get_2_character_symbols(verbose)
-		get_3_character_symbols(verbose)
-		get_4_character_symbols(verbose)
-	elif characters == '5':
-		get_1_character_symbols(verbose)
-		get_2_character_symbols(verbose)
-		get_3_character_symbols(verbose)
-		get_4_character_symbols(verbose)
-		get_5_character_symbols(verbose)
+        atotal = ysslib.get_symbols(length=int(characters), verbose=verbose,
+                                    get_names=True)
 	csv = 'yahoo_stock_symbols_%s.csv' % characters
 	txt = 'yahoo_stock_symbols_%s.txt' % characters
 	fcsv = open(csv, 'w')
 	ftxt = open(txt, 'w')
 	for s in atotal:
-		fcsv.write(s+'\n')
-		sym = s.split(',')[0]
+		fcsv.write(','.join(s)+'\n')
+		sym = s[0]
 		ftxt.write(sym+'\n')
 	fcsv.close()
 	ftxt.close()
---
+++ ysslib.py
@@ -0,0 +1,65 @@
+import urllib2, sys
+from itertools import product
+
+def get_data(url):
+   try:
+      v = urllib2.urlopen(url).read()
+   except Exception: # pylint: disable=W0703
+      return ''
+   try:
+      v = v.strip()
+   except Exception: # pylint: disable=W0703
+      return ''
+   return v
+
+def get_group(a200):
+   s = '+'.join(a200)
+   f = 'sn'
+   url = 'http://finance.yahoo.com/d/quotes.csv?s=%s&f=%s' % (s, f)
+   v = get_data(url)
+   a = v.split("\r\n")
+   group = []
+   for s in a:
+      if s.find('N/A') == -1:
+         aa = s.replace('"', '').split(',', 1)
+         try:
+            if aa[1] and aa[0] != aa[1]:
+               group.append(aa)
+         except Exception: # pylint: disable=W0703
+            continue
+   return group
+
+def _get_symbols(a1, verbose):
+   i = 0
+   j = 0
+   a200 = []
+   symbols = []
+   for e in a1:
+      try:
+         a200.append(e)
+      except Exception: # pylint: disable=W0703
+         break
+      i += 1
+      if i == 200:
+         j += 1
+         if verbose:
+            print j,
+            sys.stdout.flush()
+         symbols += get_group(a200)
+         a200 = []
+         i = 0
+   symbols += get_group(a200)
+   if verbose:
+      print "\n%d" % len(symbols)
+      sys.stdout.flush()
+   return symbols
+
+def get_symbols(length=5, verbose=False, get_names=False):
+   a1 = []
+   for n in range(1, length+1):
+      for chars in product(range(65, 65+26), repeat=n):
+         symbol = ''
+         for c in chars:
+            symbol += chr(c)
+         a1.append(symbol)
+   return _get_symbols(a1, verbose)
